3a641b3a3fac0b1491c0c4c297e3c84e730d6235
Subrata <subrata291291@hmail.com>
2025-06-19 13:44:03 +0530

    Added Aos

--- js/custom.js
+++ js/custom.js
@@ -94,3 +94,15 @@ $(".review-slider").slick({
     }
   ]
 });
+
+// Init AOS
+function aos_init() {
+  AOS.init({
+    duration: 1000,
+    easing: "ease-in-out-back",
+    once: true
+  });
+}
+$(window).on('load', function() {
+  aos_init();
+});
